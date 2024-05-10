b9de451a45e2c4fd78a4ad507a7e84317015847b
paryanial888 <paryanial888@northwested.org>
2024-05-10 14:06:50 -0400

    Created Website

---
+++ Arrow Functions/main.js
@@ -0,0 +1,23 @@
+// Alexander Paryani
+// 5/10/2024
+// JS Arrow Functions Practice
+
+// Function 1
+const studentBio = (firstName, schoolName) => `Hello! My name is ${firstName} and I go to ${schoolName}.`;
+
+const fName = prompt('What is your name:\n');
+const schoolName = prompt('What is your school name:\n');
+console.log(studentBio(fName, schoolName));
+
+// Function 2
+const totalCost = (cost1, cost2, cost3, grandTotal) => `Item One: ${cost1} | Item Two: ${cost2} | Item Three ${cost3} = ${grandTotal}`;
+const cost1 = parseFloat(prompt('Cost of amazon package 1:\n'));
+const cost2 = parseFloat(prompt('Cost of amazon package 2: \n'));
+const cost3 = parseFloat(prompt('Cost of amazon package 3: \n'));
+const grandTotal = (cost1 + cost2 + cost3).toFixed(2);
+console.log(totalCost(cost1, cost2, cost3, grandTotal));
+
+// Function 3
+const squareRoot = (num) => `The Square Root of ${num} is ${Math.sqrt(num)}.`;
+const num = parseInt(prompt('Enter a number to square root:\n'));
+console.log(squareRoot(num));
